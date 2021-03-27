4aa017326f00dae97924f862d6bdd0446336d5d2
James Dick <slightlyjames@gmail.com>
2021-03-27 08:28:26 +0000

    Alright I decided I don't like how heavily the model relied on callbacks for everything. I've decided that async/await makes this more sensible. Still seems to work pretty well and I like the code more this way so win/win!

--- scripts/controller.js
+++ scripts/controller.js
@@ -3,26 +3,27 @@
 $(() => {
     const model = new Model();
     const view = new View();
+    
+    view.setAddNoteBtnHandler(addNoteBtnHandler);
+    displayNotes();
 
-    const deleteHandler = timestamp => {
-        model.deleteNote(timestamp, deleted => {
-            view.removeNote(deleted);
-        });
-    };
-
-    view.setAddNoteBtnHandler(() => {
-        model.addNote(view.getInput(), note => {
-            view.displayNote(note, deleteHandler);
-            view.clearInput();
-        });
-    });
-
-    model.getNotes(notes => {
+    async function deleteHandler(timestamp) {
+        let deleted = await model.deleteNote(timestamp);
+        view.removeNote(deleted);
+    }
+    
+    async function addNoteBtnHandler() {
+        let newNote = await model.addNote(view.getInput());
+        view.displayNote(newNote, deleteHandler);
+        view.clearInput();
+    }
+    
+    async function displayNotes() {
+        let notes = await model.getNotes();
         if (notes[0] == undefined) {
             console.log('No notes were loaded from the db.')
             return;
         }
-        
-        view.displayNotes(notes, deleteHandler);
-    });
+        view.displayNotes(notes, deleteHandler)
+    }
 });
--- scripts/model.js
+++ scripts/model.js
@@ -1,46 +1,46 @@
 'use strict';
 
 class Model {
-    getNotes(callback) { 
-        fetch('notes/get.php', {
-            method: 'POST',
-            headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-            }
-        })
-        .then(res => res.json())
-        .then(json => {
-            console.log('Response to getting notes: ', json);
-            callback(json);
-        })
+    constructor() {
+        
     }
 
-    addNote(input, callback) {
+    async getNotes() { 
+        let res = await fetch('notes/get.php', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        let notes = await res.json();
+        return notes;
+    }
+
+    async addNote(input) {
         let note = {
             text: input,
             timestamp: Date.now(),
             colour: Math.floor(Math.random() * 6)
         };
 
-        fetch('notes/add.php', {
+        let res = await fetch('notes/add.php', {
             method: 'post',
             body: JSON.stringify(note)
-        })
-        .then(res => res.json())
-        .then(json => {
-            callback(json);
-        })
+        });
+        
+        let added = res.json();
+        return added;
     }
 
-    deleteNote(timestamp, callback) {
-        fetch('notes/delete.php', {
+    async deleteNote(timestamp) {
+        let res = await fetch('notes/delete.php', {
             method: 'post',
             body: JSON.stringify({timestamp: timestamp})
-        })
-        .then(res => res.json())
-        .then(json => {
-            callback(json.deleted);
-        })
+        });
+        
+        let deleted = await res.json();
+        return deleted.deleted;
     }
 }
